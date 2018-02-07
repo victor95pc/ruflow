0a2099d26d3845c6b443d92b222c7998007024a7
Victor Palomo de Castro <victor.palomo@fulllab.com.br>
2018-02-07 00:48:13 -0200

    Use configurations gem to store config attrs

--- lib/ruflow.rb
+++ lib/ruflow.rb
@@ -1,5 +1,18 @@
 require "ruflow/version"
+require "configurations"
 
 module Ruflow
-  # Your code goes here...
+  include Configurations
+
+  configurable :components_folder
+
+  class << self
+    def config
+      configuration
+    end
+
+    def setup(&block)
+      Ruflow.configure(&block)
+    end
+  end
 end
