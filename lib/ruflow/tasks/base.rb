b181b7f435da602bcd876b2bebd974d53a0cc8cb
Victor Palomo de Castro <victor.palomo@fulllab.com.br>
2018-02-16 03:37:05 -0200

    Add command to run a ruflow flow via command line

--- lib/ruflow/tasks/base.rb
+++ lib/ruflow/tasks/base.rb
@@ -1,14 +1,20 @@
 require_relative 'setup'
+require_relative '../../ruflow'
 
 module Ruflow
   module Tasks
     class Base < Thor
-      desc "new  [PROJECT_NAME]", "generate a new project"
+      desc "new  [PROJECT_NAME]", "Generate a new project"
       def new(project_name)
         Ruflow::Tasks::Setup.start([project_name])
       end
 
-      desc "setup", "generate all files and folder required"
+      desc "start [FLOW_CLASS_NAME]", "Start a flow"
+      def start(flow_klass_name)
+        Kernel.const_get(flow_klass_name).start
+      end
+
+      desc "setup", "Generate all files and folder required"
       def setup
         Ruflow::Tasks::Setup.start(['.'])
       end
