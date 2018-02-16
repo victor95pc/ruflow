dbd33c1c71de42055c79a6dd3531552cba710931
Victor Palomo de Castro <victor.palomo@fulllab.com.br>
2018-02-16 09:36:51 -0200

    Change example files from setup file

--- setup_files/components/actions/printer.rb
+++ setup_files/components/actions/printer.rb
@@ -1,19 +1,21 @@
-class Actions::Printer < Ruflow::Action
-  set_options(
-    default_input: '',
-    input: 'String',
-    output: {
-      ok: 'String'
-    }
-  )
+module Actions
+  class Printer < ::Ruflow::Action
+    set_options(
+      default_input: '',
+      input: 'String',
+      output: {
+        ok: 'String'
+      }
+    )
 
-  def self.with_custom_options(can_print:, text:)
-    super(can_print: can_print, text: text)
-  end
+    def self.with_custom_options(can_print:, text:)
+      super(can_print: can_print, text: text)
+    end
 
-  def start(text)
-    puts text if options[:can_print]
+    def start(text)
+      puts text if options[:can_print]
 
-    [:ok, text + options[:text]]
+      [:ok, text + options[:text]]
+    end
   end
 end
--- setup_files/components/flows/hello_world.rb
+++ setup_files/components/flows/hello_world.rb
@@ -1,10 +1,12 @@
-class Flows::HelloWorld < Ruflow:Flow
-  set_actions({
-    1 => { klass: Printer.with_custom_options(can_print: false, text: 'Hello'), output_to: { success: 2 } },
-    2 => { klass: Printer.with_custom_options(can_print: false, text: 'Hello'), output_to: { success: 3 } },
-    3 => { klass: Printer.with_custom_options(can_print: false, text: 'Again'), output_to: { success: 4 } },
-    4 => { klass: Printer.with_custom_options(can_print: true,  text: 'World') },
-  })
+module Flows
+  class HelloWorld < ::Ruflow::Flow
+    set_actions({
+      1 => { klass: ::Actions::Printer.with_custom_options(can_print: false, text: 'Hello'), output_to: { ok: 2 } },
+      2 => { klass: ::Actions::Printer.with_custom_options(can_print: false, text: 'Hello'), output_to: { ok: 3 } },
+      3 => { klass: ::Actions::Printer.with_custom_options(can_print: false, text: 'Again'), output_to: { ok: 4 } },
+      4 => { klass: ::Actions::Printer.with_custom_options(can_print: true,  text: 'World') }
+    })
 
-  set_start_action_id(1)
+    set_start_action_id(1)
+  end
 end
